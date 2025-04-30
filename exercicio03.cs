65e7ff8db2ea5974e6eafd9a9c79d454eb773121
Gabrieow <gabrielhvazsantos@gmail.com>
2025-04-30 13:46:43 -0300

    Adicionando exercício 3

---
+++ exercicio03.cs
@@ -0,0 +1,31 @@
+namespace Exercicios
+{
+    public class Fatorial
+    {
+        Console.WriteLine("Insira um número positivo: ");
+        int escolha = int.Parse(Console.ReadLine());
+
+        // tratamento de erro pra caso o usuário insira um número errado
+        if (escolha < 0)
+        {
+            Console.WriteLine("Número inválido. Insira um número positivo: ");
+            escolha = int.Parse(Console.ReadLine());
+        } 
+        else
+        {
+            int i = escolha;
+            int resultado = 1;
+
+            // enquanto i for maior que 1, vai rodar o código
+            while(i > 1)
+            {
+                // utilizando *= pra multiplicar e guardar o valor multiplicado na variável resultado
+                resultado *= i;
+                i--;
+            }
+        }
+
+        // exibe o resultado
+        Console.WriteLine($"O fatorial de {escolha} é: {resultado}");
+    }
+}
