e7eda0e539f8e4e7ca26f6c77f17bb241bb71144
Gabrieow <gabrielhvazsantos@gmail.com>
2025-04-30 13:26:21 -0300

    adicionando exercício 2

---
+++ exercicio02.cs
@@ -0,0 +1,15 @@
+namespace Exercicios
+{
+    public class TabuadaCompleta
+    {
+        Console.WriteLine("Insira um número: ");
+        int escolha = int.Parse(Console.ReadLine());
+        
+        // enquanto o contador for menor que 11, ele vai rodar
+        for(int i; i < 11; i++)
+        {
+            resultado = escolha * i;
+            Console.WriteLine($"{escolha} x {i} = {resultado}");
+        }
+    }
+}
